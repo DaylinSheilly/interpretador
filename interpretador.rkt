93697f468318a9e6ffd3c921115d78cf04e5609d
Sebastian Tombe 2110051 <johan.tombe@correounivalle.edu.co>
2022-11-07 18:54:11 -0500

    Copian el codigo de Juan Mazuera

--- interpretador.rkt
+++ interpretador.rkt
@@ -1,12 +1,4 @@
 #lang eopl
-#|
-    Integrantes:
-    César Alejandro Grijalba Zúñiga - 202110035
-    Johan Sebastian Tombe - 202110051
-    Laura Murillas Andrade - 1944153
-    Juan Esteban Mazuera - 2043008
-    Sheilly Ortega - 2040051
-|#
 
 #|
     Diseñe un interpretador para la siguiente gramática que realiza
@@ -16,8 +8,11 @@
 |#
 
 #|
+    La definición BNF para las expresiones del lenguaje:
+
     <programa> :=  <expresion> 
                un-programa (exp)
+
     <expresion> := <numero>
                 numero-lit (num)
                 := "\""<texto> "\""
@@ -27,17 +22,18 @@
                 := (<expresion> <primitiva-binaria> <expresion>)
                 primapp-bin-exp (exp1 prim-binaria exp2)
                 := <primitiva-unaria> (<expresion>)
-                primapp-un-exp (prim-unaria exp)|#
+                primapp-un-exp (prim-unaria exp)
+                := "Si" <expresion> "entonces" <expresion> "sino" <expresion> "finSi"
+                condicional-exp(test-exp true-exp false-exp)
+                := "declarar" "(" <identificador> "=" <expresion> (";") ")" "{" <expresion> "}"
+                variableLocal-exp(ids exps cuerpo)
 
-#|
     <primitiva-binaria> :=  + (primitiva-suma)
                         :=  ~ (primitiva-resta)
                         :=  / (primitiva-div)
                         :=  * (primitiva-multi)
                         :=  concat (primitiva-concat)
-|#
 
-#|
     <primitiva-unaria> :=  longitud (primitiva-longitud)
                        :=  add1 (primitiva-add1)
                        :=  sub1 (primitiva-sub1)
@@ -51,126 +47,253 @@
                      variables @x y @z son válidas
 |#
 
-#|
-    2)Defina un ambiente inicial con las variables (@a @b @c @d @e) con valores (1 2 3 "hola" "FLP")
-    y modifique su función evaluar-expresión para que acepte dicho ambiente.
-    -Diseñe una función llamada (buscar-variable) que recibe un símbolo (identificador) y un ambiente,
-    retorna el valor si encuentra la variable en el ambiente. En caso contrario: "Error, la variable 
-    no existe"
-|#
+;Especificación Léxica
 
-#|
-    Pruebe:
-    --> @a
-    1
-    --> @b
-    2
-    --> @e
-    "FLP"
-|#
+(define scanner-spec-simple-interpreter
+'((white-sp
+   (whitespace) skip)
+  (comment
+    ("%"(arbno (not #\newline))) skip)
+  ;pregunta como colocar \ \ y letras y numeros al tiempo
+  (texto
+   ("/" (arbno (or letter digit)) "/") string)
+  ;pregunta solo debe ser valido un ? y cómo se haría
+  (identificador
+   ("@" (arbno (or letter digit)) "?") symbol)
+  ; enteros positivos y negativos
+  (numero 
+   (digit (arbno digit)) number)
+  (numero 
+   ("-" digit (arbno digit)) number)
+  ; flotantes positivos y negativos
+  (numero 
+   (digit (arbno digit) "." digit (arbno digit)) number)
+  (numero 
+   ("-" digit (arbno digit) "." digit (arbno digit)) number)))
 
-#|
-    3) Implemente los Booleanos:
-    En una expresión numérica, 0 es falso, cualquier otro caso es verdadero. Para esto diseñe la
-    función valor-verdad? que realiza esta verificación.|#
+;Especificación Sintáctica (gramática)
 
-#|
-    4) Extienda la gramática con condicionales:
-    <expresion> := Si <expresion> entonces <expresion>  sino <expresion>
-    finSI condicional-exp (test-exp true-exp false-exp)
-|#
+(define grammar-simple-interpreter
+  '((program (expression) a-program)
+    (expression (numero) numero-lit)
+    (expression (texto) texto-lit)
+    (expression (identificador) var-exp)
+    (expression (primitiva-unaria "("expression")") primapp-un-exp)
+    (expression ("("expression primitiva-binaria expression")") primapp-bin-exp)
+    
+    (primitiva-unaria ("longitud") primitiva-longitud)
+    (primitiva-unaria ("add1") primitiva-add1)
+    (primitiva-unaria ("sub1") primitiva-sub1)
+    
+    (primitiva-binaria ("+") primitiva-suma)
+    (primitiva-binaria ("~") primitiva-resta)
+    (primitiva-binaria ("*") primitiva-multi)
+    (primitiva-binaria ("/") primitiva-div)
+    (primitiva-binaria ("concat") primitiva-concat)
 
-#|
-    Debe probar:
-    --> Si (2+3) entonces 2 sino 3 finSI
-    2
-    --> Si (longitud(@d) ~ 4) entonces 2 sino 3 finSI
-    3
-|#
-
-#|
-    5) Implemente declaración de variables locales:
-    <expresion> := declarar (<identificador> = <expresion> (;)) { <expresion> }
-                variableLocal-exp (ids exps cuerpo)
-|#
-
-#|
-    Debe probar:
-    --> declarar (@x=2;@y=3;@a=7){
-        (@a+(@x~@y))
-        }
-    6
-    --> declarar (@x=2;@y=3;@a=7) {
-        (@a+@b)
-        }
-   9
-|#
-
-#|
-    6) Extienda la gramática para crear procedimientos
-    <expresion> := procedimiento (<identificador>*',') haga <expresion> finProc procedimiento-ex (ids cuero)
-    Para esto debe definir un datatype para la cerradura (o ProcVal) que debe tener 3 campos:
-    1. Lista ID del procedimiento
-    2. Cuerpo del procedimiento
-    3. Ambiente donde fue declarado
-|#
-
-#|
-(define-datatype procVal procVal?
-  (cerradura
-   (lista-ID (list-of symbol?))
-   (exp expresion?)
-   (amb ambiente?)
+    (expression ("Si" expression "entonces" expression "sino" expression "finSI") condicional-exp)
+    (expression ("declarar" "(" (arbno identificador "=" expression ";") ")" "{" expression "}") variableLocal-exp)
    )
 )
-|#
 
-#|
-    Debe probar:
-    --> procedimiento (@x,@y,@z) haga ((@x+@y)+@z) finProc
-    #(struct:cerradura (@x @y @z) #(struct:primapp-bin-exp #(struct:primapp-bin-exp #(struct:var-exp
-    @x) #(struct:primitiva-sum) #(struct:var-exp @y)) #(struct:primitiva-sum) #(struct:var-exp @z)) #
-    (struct:extendido (@a @b @c @d @e) (1 2 3 "hola" "FLP") #(struct:vacio)))
-    Se debe retornar una cerradura
-|#
+;Tipos de datos para la sintaxis abstracta de la gramática
 
-#|
-    7) Extienda la gramática para evaluar procedimientos:
-    <expresion> :=  "evaluar" expresion   (expresion ",")*  finEval
-                app-exp(exp exps)
-|#
+;Construidos manualmente:
 
-#|
-    Debe probar:
-    -->  declarar (
-         @x=2;
-         @y=3;
-         @a=procedimiento (@x,@y,@z) haga ((@x+@y)+@z) finProc
-         ) {
-         evaluar @a (1,2,@x) finEval
-         }
-    5
-    --> declarar (
-        @x=procedimiento (@a,@b) haga ((@a*@a) + (@b*@b)) finProc;
-        @y=procedimiento (@x,@y) haga (@x+@y) finProc
-        ) {
-        ( evaluar @x(1,2) finEval + evaluar @y(2,3) finEval )
-        }
-        10
-    --> declarar (
-        @x= Si (@a*@b) entonces (@d concat @e) sino longitud((@d concat
-        @e)) finSI;
-        @y=procedimiento (@x,@y) haga (@x+@y) finProc
-        ) {
-        ( longitud(@x) * evaluar @y(2,3) finEval )
-          }
-    35
-|#
+;(define-datatype program program?
+;  (a-program
+;   (exp expression?)))
+;
+;(define-datatype expression expression?
+;  (lit-exp
+;   (datum number?))
+;  (var-exp
+;   (id symbol?))
+;  (primapp-exp
+;   (prim primitive?)
+;   (rands (list-of expression?)))
+;  (if-exp
+;   (test-exp expression?)
+;   (true-exp expression?)
+;   (false-exp expression?))
+;  (let-exp
+;   (ids (list-of symbol?))
+;   (rans (list-of expression?))
+;   (body expression?)))
+;
+;(define-datatype primitive primitive?
+;  (add-prim)
+;  (substract-prim)
+;  (mult-prim)
+;  (incr-prim)
+;  (decr-prim))
 
-;8) Extienda la gramática para incluir llamados recursivos. Proponga una definición en la gramática e impleméntela.
+;Construida automáticamente la sintaxis abstracta:
 
-;9) Dibuje el árbol de sintaxis abstracta de los ejercicios del punto 7.
+(sllgen:make-define-datatypes scanner-spec-simple-interpreter grammar-simple-interpreter)
 
-;10) Dibuje el diagrama de ambientes de los ejercicios del punto 7 (tenga en cuenta el ambiente inicial del punto 1).
+(define show-the-datatypes
+  (lambda () (sllgen:list-define-datatypes scanner-spec-simple-interpreter grammar-simple-interpreter)))
 
-;11) Utilización del lenguaje de programación:
+;*******************************************************************************************
+;Parser, Scanner, Interfaz
+
+;El FrontEnd (Análisis léxico (scanner) y sintáctico (parser) integrados)
+
+(define scan&parse
+  (sllgen:make-string-parser scanner-spec-simple-interpreter grammar-simple-interpreter))
+
+;El Analizador Léxico (Scanner)
+
+(define just-scan
+  (sllgen:make-string-scanner scanner-spec-simple-interpreter grammar-simple-interpreter))
+
+;El Interpretador (FrontEnd + Evaluación + señal para lectura )
+
+(define interpretador
+  (sllgen:make-rep-loop  "--❤ "
+    (lambda (pgm) (eval-program  pgm)) 
+    (sllgen:make-stream-parser 
+      scanner-spec-simple-interpreter
+      grammar-simple-interpreter)))
+
+;*******************************************************************************************
+;El Interprete
+
+;eval-program: <programa> -> numero
+; función que evalúa un programa teniendo en cuenta un ambiente dado (se inicializa dentro del programa)
+
+(define eval-program
+  (lambda (pgm)
+    (cases program pgm
+      (a-program (body)
+                 (eval-expression body (init-env))))))
+
+; Ambiente inicial
+;(define init-env
+;  (lambda ()
+;    (extend-env
+;     '(x y z)
+;     '(4 2 5)
+;     (empty-env))))
+(define init-env
+  (lambda ()
+    (extend-env
+     '(@x @y @z)
+     '(4 2 5)
+     (empty-env))))
+
+;eval-expression: <expression> <enviroment> -> numero
+; evalua la expresión en el ambiente de entrada
+(define eval-expression
+  (lambda (exp env)
+    (cases expression exp
+      (numero-lit (num) num)
+      (texto-lit (txt) txt)
+      (var-exp (id) (apply-env env id))
+      (primapp-un-exp (prim-unaria exp)
+                      (apply-un-primitive prim-unaria (eval-expression exp env)))
+      (primapp-bin-exp (exp1 prim-binaria exp2)
+                       (apply-bin-primitive (eval-expression exp1 env) prim-binaria (eval-expression exp2 env)))
+      (condicional-exp (test-exp true-exp false-exp)
+                       ((if (true-value? (eval-expression test-exp env))
+                        (eval-expression true-exp env)
+                        (eval-expression false-exp env))))
+      (variableLocal-exp (ids exps cuerpo) (0))
+      ))
+)
+; funciones auxiliares para aplicar eval-expression a cada elemento de una 
+; lista de operandos (expresiones)
+(define eval-rands
+  (lambda (rands env)
+    (map (lambda (x) (eval-rand x env)) rands)))
+
+(define eval-rand
+  (lambda (rand env)
+    (eval-expression rand env)))
+
+;apply-un-primitive: <primitiva-unaria> (<expression>) -> numero
+(define apply-un-primitive
+  (lambda (prim-unaria exp)
+    (cases primitiva-unaria prim-unaria
+      (primitiva-longitud ()(- (string-length exp) 2))
+      (primitiva-add1 () (+ exp 1))
+      (primitiva-sub1 () (- exp 1)))))
+
+;apply-bin-primitive: (<expression> <primitiva-binaria> <expression>) -> numero
+(define apply-bin-primitive
+  (lambda (exp1 prim-binaria exp2)
+    (cases primitiva-binaria prim-binaria
+      (primitiva-suma () (+ exp1 exp2))
+      (primitiva-resta () (- exp1 exp2))
+      (primitiva-multi () (* exp1 exp2))
+      (primitiva-div () (/ exp1 exp2))
+      (primitiva-concat () (string-append (number->string exp1) (number->string exp2)))
+      )))
+
+;true-value?: determina si un valor dado corresponde a un valor booleano falso o verdadero
+(define true-value?
+  (lambda (x)
+    (not (zero? x))))
+
+;*******************************************************************************************
+;Ambientes
+
+;definición del tipo de dato ambiente
+(define-datatype environment environment?
+  (empty-env-record)
+  (extended-env-record (syms (list-of symbol?))
+                       (vals (list-of scheme-value?))
+
+                       (env environment?)))
+
+(define scheme-value? (lambda (v) #t))
+
+;empty-env:      -> enviroment
+;función que crea un ambiente vacío
+(define empty-env  
+  (lambda ()
+    (empty-env-record)))       ;llamado al constructor de ambiente vacío 
+
+
+;extend-env: <list-of symbols> <list-of numbers> enviroment -> enviroment
+;función que crea un ambiente extendido
+(define extend-env
+  (lambda (syms vals env)
+    (extended-env-record syms vals env))) 
+
+;función que busca un símbolo en un ambiente
+(define apply-env
+  (lambda (env sym)
+    (cases environment env
+      (empty-env-record ()
+                        (eopl:error 'apply-env "No binding for ~s" sym))
+      (extended-env-record (syms vals env)
+                           (let ((pos (list-find-position sym syms)))
+                             (if (number? pos)
+                                 (list-ref vals pos)
+                                 (apply-env env sym)))))))
+
+
+;****************************************************************************************
+;Funciones Auxiliares
+
+; funciones auxiliares para encontrar la posición de un símbolo
+; en la lista de símbolos de unambiente
+
+(define list-find-position
+  (lambda (sym los)
+    (list-index (lambda (sym1) (eqv? sym1 sym)) los)))
+
+(define list-index
+  (lambda (pred ls)
+    (cond
+      ((null? ls) #f)
+      ((pred (car ls)) 0)
+      (else (let ((list-index-r (list-index pred (cdr ls))))
+              (if (number? list-index-r)
+                (+ list-index-r 1)
+                #f))))))
+
+;******************************************************************************************
+;EJERCICIOS
