8ed484f2a2d4bd87e0c64addebcdfcef421cb6a2
Sheilly Ortega 2040051 <sheilly.ortega@correounivalle.edu.co>
2022-11-08 14:42:09 -0500

    implementado el espacio en blanco en el texto

--- interpretador.rkt
+++ interpretador.rkt
@@ -40,36 +40,6 @@
                      variables @x y @z son válidas
 |#
 
-;especificacion lexica 
-
-(define scaner-spec
-'(
-    (white-sp (whitespace) skip)
-    (comment ("//" (arbno (not #\newline))) skip)
-    (identificador 
-    ("@" (arbno (or letter digit)) "?") symbol)
-    (texto("/" (arbno (or letter digit)) "/") string)
-    ; enteros positivos y negativos
-    (numero (digit (arbno digit)) number)
-    (numero ("-" digit (arbno digit)) number)
-    ; flotantes positivos y negativos
-    (numero (digit (arbno digit) "." digit (arbno digit)) number)
-    (numero ("-" digit (arbno digit) "." digit (arbno digit)) number)
-)
-)
-
-;especificacion sintactica 
-
-(define grammar-simple-interpreter
-  '((program (expression) a-program)
-    (expression (numero) numero-lit)
-    (expresion (texto) texto-lit)
-    (expression (identificador) var-exp)
-    (expression (primitiva-unaria "("expression")") primapp-un-exp)
-    (primitiva-unaria ("longitud") primitiva-longitud)
-    (primitiva-unaria ("add1") primitiva-add1)
-    (primitiva-unaria ("sub1") primitiva-sub1)
-    ))
 ;Especificación Léxica
 
 (define scanner-spec-simple-interpreter
@@ -78,17 +48,21 @@
   (comment
     ("%"(arbno (not #\newline))) skip)
   ;pregunta como colocar \ \ y letras y numeros al tiempo
-  (texto("/" (arbno (or letter digit)) "/") string)
+  (texto
+   ("\"" (arbno (or letter digit whitespace)) "\"") string)
   ;pregunta solo debe ser valido un ? y cómo se haría
   (identificador
-   ("@"
-    (arbno (or letter digit)) "?") symbol)
+   ("@" (arbno (or letter digit)) "?") symbol)
   ; enteros positivos y negativos
-  (numero (digit (arbno digit)) number)
-  (numero ("-" digit (arbno digit)) number)
+  (numero 
+   (digit (arbno digit)) number)
+  (numero 
+   ("-" digit (arbno digit)) number)
   ; flotantes positivos y negativos
-  (numero (digit (arbno digit) "." digit (arbno digit)) number)
-  (numero ("-" digit (arbno digit) "." digit (arbno digit)) number)))
+  (numero 
+   (digit (arbno digit) "." digit (arbno digit)) number)
+  (numero 
+   ("-" digit (arbno digit) "." digit (arbno digit)) number)))
 
 ;Especificación Sintáctica (gramática)
 
@@ -98,15 +72,10 @@
     (expression (texto) texto-lit)
     (expression (identificador) var-exp)
     (expression (primitiva-unaria "("expression")") primapp-un-exp)
-    ;;;;;;
     (primitiva-unaria ("longitud") primitiva-longitud)
     (primitiva-unaria ("add1") primitiva-add1)
     (primitiva-unaria ("sub1") primitiva-sub1)
-  ; características adicionales
-    (expression ("Si" expression "entonces" expression "sino" expression "finSI")
-                condicional-exp)
-    (expression ("let" (arbno identificador "=" expression) "in" expression)
-                let-exp))
+   )
 )
 
 ;Tipos de datos para la sintaxis abstracta de la gramática
@@ -204,14 +173,6 @@
       (numero-lit (num) num)
       (texto-lit (txt) txt)
       (var-exp (id) (apply-env env id))
-      (condicional-exp (test-exp true-exp false-exp)
-              (if (true-value? (eval-expression test-exp env))
-                  (eval-expression true-exp env)
-                  (eval-expression false-exp env)))
-      (let-exp (ids rands body)
-               (let ((args (eval-rands rands env)))
-                 (eval-expression body
-                                  (extend-env ids args env))))
       (primapp-un-exp (prim-unaria exp)
                       (apply-un-primitive prim-unaria (eval-expression exp env)))
       ))
@@ -301,135 +262,3 @@
 ;******************************************************************************************
 ;EJERCICIOS
     
-
-
-
-
-
-
-#|
-    2)Defina un ambiente inicial con las variables (@a @b @c @d @e) con valores (1 2 3 "hola" "FLP")
-    y modifique su función evaluar-expresión para que acepte dicho ambiente.
-    -Diseñe una función llamada (buscar-variable) que recibe un símbolo (identificador) y un ambiente,
-    retorna el valor si encuentra la variable en el ambiente. En caso contrario: "Error, la variable 
-    no existe"
-|#
-
-#|
-    Pruebe:
-    --> @a
-    1
-    --> @b
-    2
-    --> @e
-    "FLP"
-|#
-
-#|
-    3) Implemente los Booleanos:
-    En una expresión numérica, 0 es falso, cualquier otro caso es verdadero. Para esto diseñe la
-    función valor-verdad? que realiza esta verificación.
-|#
-
-#|
-    4) Extienda la gramática con condicionales:
-    <expresion> := Si <expresion> entonces <expresion>  sino <expresion>
-    finSI condicional-exp (test-exp true-exp false-exp)
-|#
-
-#|
-    Debe probar:
-    --> Si (2+3) entonces 2 sino 3 finSI
-    2
-    --> Si (longitud(@d) ~ 4) entonces 2 sino 3 finSI
-    3
-|#
-
-#|
-    5) Implemente declaración de variables locales:
-    <expresion> := declarar (<identificador> = <expresion> (;)) { <expresion> }
-                variableLocal-exp (ids exps cuerpo)
-|#
-
-#|
-    Debe probar:
-    --> declarar (@x=2;@y=3;@a=7){
-        (@a+(@x~@y))
-        }
-    6
-    --> declarar (@x=2;@y=3;@a=7) {
-        (@a+@b)
-        }
-   9
-|#
-
-
-
-#|
-    6) Extienda la gramática para crear procedimientos
-    <expresion> := procedimiento (<identificador>*',') haga <expresion> finProc procedimiento-ex (ids cuero)
-    Para esto debe definir un datatype para la cerradura (o ProcVal) que debe tener 3 campos:
-    1. Lista ID del procedimiento
-    2. Cuerpo del procedimiento
-    3. Ambiente donde fue declarado|#
-
-#|
-
-(define-datatype procVal procVal?
-  (cerradura
-   (lista-ID (list-of symbol?))
-   (exp expresion?)
-   (amb ambiente?)
-   )
-)
-|#
-
-#|
-    Debe probar:
-    --> procedimiento (@x,@y,@z) haga ((@x+@y)+@z) finProc
-    #(struct:cerradura (@x @y @z) #(struct:primapp-bin-exp #(struct:primapp-bin-exp #(struct:var-exp
-    @x) #(struct:primitiva-sum) #(struct:var-exp @y)) #(struct:primitiva-sum) #(struct:var-exp @z)) #
-    (struct:extendido (@a @b @c @d @e) (1 2 3 "hola" "FLP") #(struct:vacio)))
-    Se debe retornar una cerradura
-|#
-
-#|
-    7) Extienda la gramática para evaluar procedimientos:
-    <expresion> :=  "evaluar" expresion   (expresion ",")*  finEval
-                app-exp(exp exps)
- |#
-
- #|
-    Debe probar:
-    -->  declarar (
-         @x=2;
-         @y=3;
-         @a=procedimiento (@x,@y,@z) haga ((@x+@y)+@z) finProc
-         ) {
-         evaluar @a (1,2,@x) finEval
-         }
-    5
-    --> declarar (
-        @x=procedimiento (@a,@b) haga ((@a*@a) + (@b*@b)) finProc;
-        @y=procedimiento (@x,@y) haga (@x+@y) finProc
-        ) {
-        ( evaluar @x(1,2) finEval + evaluar @y(2,3) finEval )
-        }
-        10
-    --> declarar (
-        @x= Si (@a*@b) entonces (@d concat @e) sino longitud((@d concat
-        @e)) finSI;
-        @y=procedimiento (@x,@y) haga (@x+@y) finProc
-        ) {
-        ( longitud(@x) * evaluar @y(2,3) finEval )
-          }
-    35
-|#
-
-;8) Extienda la gramática para incluir llamados recursivos. Proponga una definición en la gramática e impleméntela.
-
-;9) Dibuje el árbol de sintaxis abstracta de los ejercicios del punto 7.
-
-;10) Dibuje el diagrama de ambientes de los ejercicios del punto 7 (tenga en cuenta el ambiente inicial del punto 1).
-
-;11) Utilización del lenguaje de programación:
